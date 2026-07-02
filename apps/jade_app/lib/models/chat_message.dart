1b513fbea7199aa9b369a0f9034fc8166a5753af
thedorfer <thedorfer@gmail.com>
2026-07-02 16:49:32 -0500

    Enhance Jade chat message metadata

--- apps/jade_app/lib/models/chat_message.dart
+++ apps/jade_app/lib/models/chat_message.dart
@@ -3,12 +3,14 @@ class ChatMessage {
   final String text;
   final String? confidenceLabel;
   final String? action;
+  final List<String> sources;
 
   ChatMessage({
     required this.role,
     required this.text,
     this.confidenceLabel,
     this.action,
+    this.sources = const [],
   });
 
   bool get isUser => role == 'user';
