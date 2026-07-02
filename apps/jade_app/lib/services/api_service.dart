9665c4cfa3367e03cdbdc77882cb5afd79406ae6
thedorfer <thedorfer@gmail.com>
2026-07-02 17:19:38 -0500

    Add Jade mode and dashboard API calls

--- apps/jade_app/lib/services/api_service.dart
+++ apps/jade_app/lib/services/api_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/chat_message.dart';
+import '../models/dashboard_card.dart';
 import '../models/jade_settings.dart';
 
 class ApiService {
@@ -9,14 +10,29 @@ class ApiService {
     return {'statusCode': res.statusCode, 'body': res.body};
   }
 
-  Future<ChatMessage> ask(JadeSettings settings, String question) async {
+  Future<List<DashboardCard>> dashboard(JadeSettings settings, String mode) async {
+    final res = await http.get(
+      Uri.parse('${settings.apiBase}/dashboard?mode=$mode'),
+      headers: {'X-JamesOS-Key': settings.apiKey},
+    );
+
+    final data = jsonDecode(res.body);
+    final cards = data['cards'];
+    if (cards is! List) return [];
+    return cards
+        .whereType<Map>()
+        .map((item) => DashboardCard.fromJson(Map<String, dynamic>.from(item)))
+        .toList();
+  }
+
+  Future<ChatMessage> ask(JadeSettings settings, String question, {String mode = 'personal'}) async {
     final res = await http.post(
       Uri.parse('${settings.apiBase}/ask'),
       headers: {
         'Content-Type': 'application/json',
         'X-JamesOS-Key': settings.apiKey,
       },
-      body: jsonEncode({'question': question, 'use_ai': settings.useAi}),
+      body: jsonEncode({'question': question, 'use_ai': settings.useAi, 'mode': mode}),
     );
 
     final data = jsonDecode(res.body);
