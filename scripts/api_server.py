12f80257bfe3a483b606c35cd6d070f4f1f7cf79
thedorfer <thedorfer@gmail.com>
2026-07-04 09:56:10 -0500

    Mount memory explorer routes

--- scripts/api_server.py
+++ scripts/api_server.py
@@ -1,7 +1,12 @@
 import uvicorn
 
+from jamesos.core.api import app
+from jamesos.core.memory_routes import router as memory_router
+
+app.include_router(memory_router)
+
 uvicorn.run(
-    "jamesos.core.api:app",
+    app,
     host="0.0.0.0",
     port=8787,
     reload=False,
