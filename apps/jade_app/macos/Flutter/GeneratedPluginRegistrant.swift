bc737d46dc3d336a178bf401d0ad6594ed436b09
James Allendoerfer <thedorfer@gmail.com>
2026-07-04 10:26:50 -0500

    Update Jade desktop plugin registrants

--- apps/jade_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/jade_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,13 @@
 import FlutterMacOS
 import Foundation
 
-import file_picker
+import file_selector_macos
 import flutter_tts
 import shared_preferences_foundation
 import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
